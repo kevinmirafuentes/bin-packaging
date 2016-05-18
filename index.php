e6c4f45c5805c40360209b5534b254c95ba08a17
Kevin Mirafuentes <kevin.mirafuentes@unarealidad.com>
2016-05-18 18:08:56 +0800

    revised

--- index.php
+++ index.php
@@ -1,54 +1,193 @@
 <?php
 
-include 'Packer.php';
-include 'Item.php';
-include 'Box.php';
+$items = array(
+    'a' => array('length' => 10, 'width' => 20, 'height' => 5, 'quantity' => 5,  'weight' => 0.5),
+    'b' => array('length' => 13, 'width' => 10, 'height' => 16, 'quantity' => 3, 'weight' => 1),
+);
 
-// calculate total order volume
-// get smallest classes which has greater volume than order
-    // sort shipping classes from biggest dimensions
-    // get the smallest classes which has greater volume than order
-        // when order cannot fit any shipping any classes, split order and try again
-// get the cheapest transport method based on shipping classes volume
+$packages = array(
+    'p1' => array('length' => 30, 'width' => 20, 'height' => 15, 'weight' => 5),
+);
 
-// sort from biggest to smallest
 
-$test = new Packer();
-foreach ($x = 0; $x < 3; $x++) {
-    $test->addItem(new Item('Product 1', 30, 20, 10));    
+class Item
+{
+    public $length;
+    public $width;
+    public $height;
+    public $weight;
+    public $quantity;
+
+    public $dimensions;
+
+    public function __construct($length, $width, $height, $weight, $quantity = 1)
+    {
+        $this->weight = $weight;
+        $this->quantity = $quantity;
+
+        $tmp = array($length, $width, $height);
+        rsort($tmp);
+
+        $this->length = $tmp[0];
+        $this->width  = $tmp[1];
+        $this->height = $tmp[2];
+    }
 }
 
-foreach ($x = 0; $x < 5; $x++) {
-    $test->addItem(new Item('Product 2', 20, 5, 2));    
+class ItemList
+{
+    public $items;
+
+    public function addItem(Item $item)
+    {
+        $this->items = $item;
+    }
+
+    public function getTotalVolume()
+    {
+        $totalVolume = 0;
+        foreach ($this->items as $item) {
+            $totalVolume += $item->length * $item->width * $this->height;
+        }
+        return $totalVolume;
+    }
+
+    public function getTotalWeight()
+    {
+        $totalWeight = 0;
+        foreach ($this->items as $item) {
+            $totalWeight += $item->weight;
+        }
+        return $totalWeight;
+    }
+
+    public function getTotalLength()
+    {
+        $totalLength = 0;
+        foreach ($this->items as $item) {
+            $totalLength += $item->length;
+        }
+        return $totalLength;
+    }
+
+    public function getTotalWidth()
+    {
+        $totalWidth = 0;
+        foreach ($this->items as $item) {
+            $totalWidth += $item->width;
+        }
+        return $totalWidth;
+    }
+
+    public function getLongestItemLength()
+    {
+        $lengths = array();
+        foreach ($this->items as $item) {
+            $lengths[] = $item->lenght;
+        }
+        return max($lengths);
+    }
+
+    public function getLongestItemWidth()
+    {
+        $widths = array();
+        foreach ($this->items as $item) {
+            $widths[] = $item->width;
+        }
+        return max($widths);
+    }
+
+    public function getLongestItemHeight()
+    {
+        $heights = array();
+        foreach ($this->items as $item) {
+            $heights[] = $item->height;
+        }
+        return max($heights);
+    }
 }
 
-foreach ($x = 0; $x < 5; $x++) {
-    $test->addItem(new Item('Product 3', 5, 5, 5));    
+class Package
+{
+    public $lenght;
+    public $width;
+    public $height;
+    public $maxWeight;
+    public $contents;
+
+    public function __construct($length, $width, $height, $weight)
+    {
+        $this->maxWeight = $weight;
+
+        $tmp = array($length, $width, $height);
+        rsort($tmp);
+
+        $this->length = $tmp[0];
+        $this->width  = $tmp[1];
+        $this->height = $tmp[2];
+    }
+
+    public function getVolume()
+    {
+        return $this->length * $this->width * $this->height;
+    }
+
+    public function canPack(ItemList $itemlist)
+    {
+        if ($itemlist->getTotalVolume() > $this->getVolume()) {
+            return false;
+        }
+
+        if ($itemlist->getTotalWeight() > $this->maxWeight) {
+            return false;
+        }
+
+        // check if can fit in normal orientations
+        if ($itemlist->getTotalLength() <= $this->length &&
+            $itemlist->getLongestItemWidth() <= $this->width &&
+            $itemlist->getLongestItemHeight() <= $this->height) {
+            $this->contents = $itemlist->items;
+            return true;
+        }
+
+        // check if can fit in rotated orientations
+        if ($itemlist->getTotalWidth() <= $this->length &&
+            $itemlist->getLongestItemLength() <= $this->width &&
+            $itemlist->getLongestItemHeight() <= $this->height) {
+            $this->contents = $itemlist->items;
+            return true;
+        }
+
+        // try to fit items one by one
+        $rlength = $this->lenght;
+        $rheight = $this->height;
+        $rwidth  = $this->width;
+
+        foreach ($itemlist->items as $item) {
+            if ($rheight < $item->height) {
+                return false;
+            }
+            // normal orientation
+            if ($rlength >= $item->length && $rwidth >= $item->width) {
+                $rlength -= $item->length;
+                $rwidth  -= $item->width;
+                if ($rheight > ($this->height - $item->height)) {
+                    $rheight = $this->height - $item-$this->height;
+                }
+                $this->contents[] = $item;
+            }
+            // try rotated orientation
+            if ($rlength >= $item->width && $rwidth >= $item->length) {
+                $rlength -= $item->width;
+                $rwidth  -= $item->length;
+                if ($rheight > ($this->height - $item->height)) {
+                    $rheight = $this->height - $item-$this->height;
+                }
+                $this->contents[] = $item;
+            }
+        }
+
+        return count($this->contents) == count($itemlist->items);
+    }
 }
 
-$test->addBox(new Box('small bag', 10, 10, 5));
-$test->addBox(new Box('large bag', 20, 20, 10));
-$test->addBox(new Box('small box', 30, 30, 20));
-$test->addBox(new Box('large box', 40, 40, 30));
-
-$test->pack();
-
-
-// $order = array(
-//     0 => array('name' => 'product 1', 'qty' => 3, 'dimension' => '10x20x30'),
-//     1 => array('name' => 'product 2', 'qty' => 5, 'dimension' => '20x2x5'),
-//     2 => array('name' => 'product 3', 'qty' => 10, 'dimension' => '5x5x5'),
-// );
-
-// $shippingClasses = array(
-//     0 => array('name' => 'small bag', 'dimension' => '10x10x5'),
-//     1 => array('name' => 'large bag', 'dimension' => '20x20x10'),
-//     2 => array('name' => 'small box', 'dimension' => '30x40x20'),
-//     3 => array('name' => 'large box', 'dimension' => '40x40x30'),
-// );
-
-// $transportMethod = array(
-//     0 => array('name' => 'freighter', 'volume' => 1920000, 'cost' => 30),
-//     1 => array('name' => 'dump truck', 'volume' => 1440000, 'cost' => 30),
-//     2 => array('name' => 'pellet truck', 'volume' => 100000, 'cost' => 30),
-// );
